b585c8ea6b979fea4b20ca81b2b91e80bf2f06e2
Josue Vargas Zúñiga <jovz1798@gmail.com>
2022-05-31 02:44:25 +0000

    domain name generator app

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domains = [".com", ".net", ".us", ".io"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateDomainNames = (pron, adj, noun, domain) => {
+  for (let i = 0; i < adj.length; i++) {
+    for (let j = 0; j < pron.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          // DOM output
+          let list = document.querySelector("#list");
+          let listItem = document.createElement("listItem");
+          listItem.innerHTML = `${pron[j] +
+            adj[i] +
+            noun[k] +
+            domain[l]} </br>`;
+          list.appendChild(listItem);
+          //console output
+          console.log(pron[j] + adj[i] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+};
+
+window.onload = () => {
+  generateDomainNames(pronoun, adj, noun, domains);
 };
